9a4569f36092ddf5c39a283f897a281be7458792
Cade Simpson <55095557+Cadesius@users.noreply.github.com>
2022-04-06 11:25:28 +0100

    Version 0.2
    Fixed multiple bugs, focused program further on optical media, optimised ripping speed, added ASCII art loader.

--- 1-CD-R.bat
+++ 1-CD-R.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
 
-@REM Intro text 
-ECHO 1-Click Disc Ripper by Cade Simpson
-ECHO Released under The GNU General Public License (GPL) Version 2, June 1991
-ECHO:
+@REM Print intro information
+CALL data/intro.bat
 
 @REM Assign variables for disc drive letter and storage directory
 SET /P DriveLetter=Input disc drive letter: 
@@ -20,7 +18,7 @@ IF EXIST "%Storage%\" (
 
 @REM Gives user the option of creating their chosen directory if it does not already exist (defaults to program end if the user chooses not to)
 :CREATING
-CHOICE /M "The directory you have chosen does not exist, would you like to create it"
+CHOICE /N /M "The directory you have chosen does not exist, would you like to create it? (Y/N)"
 IF %ERRORLEVEL% EQU 1 GOTO :MAKEDIR
 IF %ERRORLEVEL% EQU 2 GOTO :END
 
@@ -34,6 +32,10 @@ ECHO:
 :NAMING
 SET /P DiscName=Input disc name: 
 ECHO:
+CHOICE /C dc /N /M "Is this disc a CD (C) or a DVD (D)? (C/D)"
+IF ERRORLEVEL ==1 SET BlockSize=32768
+IF ERRORLEVEL ==2 SET BlockSize=2048
+ECHO:
 
 @REM Pauses before ripping process begins
 <nul set /p "=Insert disc, then press any key to continue..."
@@ -46,7 +48,7 @@ ECHO Disc information:
 powershell "cd %DriveLetter%:; ls -r | measure -sum Length"
 
 @REM Executes dd.exe for ripping and packing into image file
-dd-0.5\dd.exe bs=4096 if="\\.\%DriveLetter%:" of="\\.\%Storage%\%DiscName%.iso" --progress
+dd-0.5\dd.exe bs=%BlockSize% if="\\.\%DriveLetter%:" of="\\.\%Storage%\%DiscName%.iso" --progress
 
 @REM Confimation message
 ECHO:
@@ -54,13 +56,12 @@ ECHO Disc ripped!
 ECHO:
 
 @REM Display popup to notify user that ripping process is completed
-
 powershell -Command "Add-Type -AssemblyName  System.Windows.Forms;$global:balloon = New-Object System.Windows.Forms.NotifyIcon;$path = (Get-Process -id $pid).Path;$balloon.Icon  = [System.Drawing.Icon]::ExtractAssociatedIcon('data/notif_icon.ico');$balloon.BalloonTipText  = 'Ripping of %DiscName% from drive %DriveLetter%: completed';$balloon.BalloonTipTitle  = '1-CD-R Notification';$balloon.Visible  = $true;$balloon.ShowBalloonTip(5000)"
 
 @REM Gives user the option to eject the relevant disc tray (or removable media) before exiting the program
 :END
 ECHO:
-CHOICE /M "Would you like to eject the disc tray before exiting"
+CHOICE /N /M "Would you like to eject the disc tray before exiting? (Y/N)"
 IF %ERRORLEVEL% EQU 1 GOTO :EJECT
 IF %ERRORLEVEL% EQU 2 GOTO :FINISH
 
